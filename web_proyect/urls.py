dd94977c5fb1efd82366fd48d8d3932bca7b9ba2
PriscilaTapia <46427698+PriscilaTapia@users.noreply.github.com>
2021-08-16 12:07:09 -0400

    Add files via upload

--- web_proyect/urls.py
+++ web_proyect/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("app_web/", include("app_web.urls")),
+    path("app_web/", include("app_web.urls")),#home
     path('admin/', admin.site.urls)
 ]
